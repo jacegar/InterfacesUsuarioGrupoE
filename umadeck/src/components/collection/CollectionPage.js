75026716493628f13ab3ae1b91ed6f9edd55ca03
juanacev <juanacevedomalaga@uma.es>
2025-04-15 14:22:28 +0200

    Imports de cartas arreglado (en principio)

--- umadeck/src/components/collection/CollectionPage.js
+++ umadeck/src/components/collection/CollectionPage.js
@@ -1,23 +1,15 @@
 import GoBackArrow from "../common/GoBackArrow";
 import Card from "../common/Card";
 import CardModel from "../../gamelogic/CardModel";
-import { useEffect, useState } from "react";
 
 function CollectionPage() {
-  const [card,setCard] = useState(new CardModel());
-
-  //Aqui solo lee una carta, pero deberia leer todas las cartas de la coleccion y mostrarlas en la lista
-  useEffect(() => {
-    setCard(new CardModel(1)); // Cargar la carta con id 1
-  }, []);
-
   return (
     <div className="collection-page">
       <GoBackArrow/>  
       <h1>Collection Page</h1>
       <ul>
         <li>{/*En cada li deberia ir un componente de la clase carta*/}</li>
-        <li><Card cardModel={card}/></li>
+        <li><Card cardModel={new CardModel(1)}/></li>
       </ul>
     </div>
   );
--- umadeck/src/components/newGame/NewGamePage.js
+++ umadeck/src/components/newGame/NewGamePage.js
@@ -1,21 +1,25 @@
 import GoBackArrow from "../common/GoBackArrow";
-import { useState } from "react";
+import { useState, useEffect } from "react"; // Add useEffect
 import Card from "../common/Card";
 import CardModel from "../../gamelogic/CardModel";
 
 //Desde esta pagina se deberia poder elegir las cartas para la nueva partida
-function NewGamePage(){
+function NewGamePage() {
     const [selectedCards, setCards] = useState([]);
 
     return (
         <div className="newGamePage">
-            <GoBackArrow/>
+            <GoBackArrow />
             <div>
                 <h1>Elige las cartas del equipo:</h1>
                 <h1>{selectedCards.length} de 3</h1>
             </div>
             <ul className="cardList">
-                <li><Card cardModel = {new CardModel(1)}/></li>
+                {loadedCards.map((card, index) => (
+                    <li key={index}>
+                        <Card cardModel={card} />
+                    </li>
+                ))}
             </ul>
         </div>
     );
