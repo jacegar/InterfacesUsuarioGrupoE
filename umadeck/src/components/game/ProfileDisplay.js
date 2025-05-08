4c5137130b55c61dcff79c96bf3a257776d4dfc9
Eduardo <earizaabad@gmail.com>
2025-05-08 19:41:34 +0200

    Nombre de usuario y rival

--- umadeck/src/components/game/ProfileDisplay.js
+++ umadeck/src/components/game/ProfileDisplay.js
@@ -2,12 +2,12 @@ import React from 'react';
 import "../styles/game/ProfileDisplay.css";
 
 function ProfileDisplay(props) {
-    const { side } = props;
+    const { side, username = side === 0 ? "Tú" : "Rival" } = props;
 
     return (
         <div className={"profile-display " + (side === 0 ? "player" : "enemy")}>
             <img src="/assets/images/image6.png" alt="PFP"></img>
-            <p>Username</p>
+            <p>{username}</p>
         </div>
     );
 }
