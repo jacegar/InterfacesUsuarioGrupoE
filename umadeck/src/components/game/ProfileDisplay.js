d0126e717370bbb8d593fbdd979caa54a86a5933
Eduardo <earizaabad@gmail.com>
2025-05-08 19:51:54 +0200

    Pinchar en una foto la muestra en grande
    Vi un chaval en la evaluacion pinchando  pensando que podia hacer algo

--- umadeck/src/components/game/ProfileDisplay.js
+++ umadeck/src/components/game/ProfileDisplay.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "../styles/game/ProfileDisplay.css";
 
 function ProfileDisplay(props) {
     const { side, username = side === 0 ? "Tú" : "Rival" } = props;
+    const [isEnlarged, setIsEnlarged] = useState(false);
+
+    const handleImageClick = () => {
+        setIsEnlarged(true);
+    };
+
+    const handleOverlayClick = () => {
+        setIsEnlarged(false);
+    };
 
     return (
-        <div className={"profile-display " + (side === 0 ? "player" : "enemy")}>
-            <img src="/assets/images/image6.png" alt="PFP"></img>
-            <p>{username}</p>
-        </div>
+        <>
+            <div className={"profile-display " + (side === 0 ? "player" : "enemy")}>
+                <img src="/assets/images/image6.png" alt="PFP" onClick={handleImageClick}></img>
+                <p>{username}</p>
+            </div>
+            {isEnlarged && (
+                <div className="profile-overlay" onClick={handleOverlayClick}>
+                    <img className="profile-enlarged" src="/assets/images/image6.png" alt="PFP Enlarged" />
+                </div>
+            )}
+        </>
     );
 }
 
